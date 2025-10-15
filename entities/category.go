952667a25e05564a8ab38147a1dc439bd5216aac
Rizqi Fathi Rafa <rizqirafa45@gmail.com>
2025-10-15 15:14:31 +0700

    feat(add) : entities

---
+++ entities/category.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `gorm:"type:varchar(100);not null" json:"name"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
